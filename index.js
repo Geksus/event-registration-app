b21ff18e7125c6a1f30be3bedbee0faabeb8dc9b
Geksus <alexgergel@gmail.com>
2024-09-26 00:09:24 +0300

    moved to another port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const organizersRoutes = require("./back/routes/organizersRoutes");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || "5000";
 
 app.use(cors());
 
